17a22ddc2962595a2c3bf8abda5fe7cb035591a0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-03-28 17:40:01 +0100

    Update output redirection in puppeteer function
    Refactor output redirection in puppeteer function.

--- autopuppeteer.sh
+++ autopuppeteer.sh
@@ -17,11 +17,11 @@ exec 5< "$puppeteer_out"
 puppeteer() { # TODO try with custom eval function to suppress the ... and | in the recoverable object and via custom eval function to avoiud the sleep 5
   \stdbuf -oL sed -E 's/(\.\.\.|\|) //g' < "$puppeteer_out" & puppeteer_out_pid="$!"
   tr '\n' '§' | sed -E 's/§(\)|\}|\]| )/\1/g' | tr '§' '\n' | while IFS=$'\n' read -r line; do sleep 15; printf '%s\n' "$line" > "$puppeteer_in"; done # node is weird
-  # exec 3>&2
-  # exec 2> /dev/null
+  exec 3>&2
+  exec 2> /dev/null
   sleep 15 && kill -9 "$puppeteer_out_pid" && { wait "$puppeteer_out_pid" || true; }
-  # exec 2>&3
-  # exec 3>&-
+  exec 2>&3
+  exec 3>&-
 }
 
 loggify_in() {
