25c1ad183824a56db779f23e3752098fa9c76410
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-05-29 18:57:24 +0200

    Fix curl command options in autopuppeteer.sh

--- autopuppeteer.sh
+++ autopuppeteer.sh
@@ -127,17 +127,16 @@ jq << EOF -Rs '{ "type": "message", "role": "assistant", "content": . }' | tee -
 await page.goto('$URL', { waitUntil: 'networkidle2', });
 // console.log(await page.content());
 EOF
-set -x
 while ( ! jq < "$conversation" 'select(.type == "message") | select(.role == "assistant") | if .content | type == "string" then .content else .content[] | select(.type == "output_text") | .text end' -r | grep -F '// DONE ' > /dev/null ) && [ "$(jq < "$conversation" -s length)" -lt "${MAX_ITERATIONS:-250}" ]; do
   if [ -n "${GUARDRAIL_STRINGS:-}" ] && jq < "$conversation" '.content' -r | grep -qF -- "$GUARDRAIL_STRINGS"; then exit 2; fi
   if [ -n "${GUARDRAIL_PATTERNS:-}" ] && jq < "$conversation" '.content' -r | grep -qE -- "$GUARDRAIL_PATTERNS"; then exit 3; fi
   jq < "$conversation" -s 'del(.['"$intro_count"':-'"${MEMORY:-100}"']) | .[]' \
     | jq -s '{ "input": ., "service_tier": "'"${OPENAI_SERVICE_TIER:-flex}"'", "model": "'"${OPENAI_MODEL:-gpt-5}"'", "reasoning": { "effort": "'"${OPENAI_REASONING_EFFORT:-high}"'" }, tools: [ { type: "web_search", search_context_size: "high" } ] }' \
     | if [ -n "${OPENAI_API_TOKEN:-}" ]; then
-      curl --no-progress-meter --fail --retry 16 --max-time "$((60 * 60))" https://api.openai.com/v1/responses -H "Authorization: Bearer $OPENAI_API_TOKEN" -H "Content-Type: application/json" --data-binary @-
+      curl --no-progress-meter --fail --max-time "$((60 * 60))" https://api.openai.com/v1/responses -H "Authorization: Bearer $OPENAI_API_TOKEN" -H "Content-Type: application/json" --data-binary @-
     elif [ -n "${GITHUB_TOKEN:-}" ]; then
       jq '{ "messages": [ .input[] | select(.type == "message") | { "role": .role, "content": ( if .content | type == "string" then .content else [ .content[] | if .type == "input_text" then { "type": "text", "text": .text } elif .type == "input_image" then { "type": "image_url", "image_url": { "url": .image_url } } else null end ] end ) } ], "service_tier": .service_tier, "model": ( "openai/" + .model ), "reasoning_effort": .reasoning.effort, "response_format": .text.format }' \
-        | curl --no-progress-meter --fail --retry 4 --max-time "$((60 * 60))" https://models.github.ai/inference/chat/completions -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/json" --data-binary @- \
+        | curl --no-progress-meter --fail --max-time "$((60 * 60))" https://models.github.ai/inference/chat/completions -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/json" --data-binary @- \
         | jq '.choices[0].message | { output: [ { "type": "message", "role": .role, content: [ { type: "output_text", text: .content } ] } ] }'
     else
       cat > /dev/null
